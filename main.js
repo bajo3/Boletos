f25d274a74991fa121476d88ed64eed783977b9f
Felipe Lentini <99766298+bajo3@users.noreply.github.com>
2023-10-28 09:49:58 -0300

    terminado

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
     const generarContratoBtn = document.getElementById("generarContrato");
     const contratoContainer = document.getElementById("contratoContainer");
+    const fecha = new Date();
+    const dia = fecha.getDate();
+    const mes = fecha.getMonth() + 1;
+    const anio = fecha.getFullYear();
     const imprimirContratoBtn = document.getElementById("imprimirContrato");
 
     generarContratoBtn.addEventListener("click", function () {
@@ -14,13 +18,16 @@ document.addEventListener("DOMContentLoaded", function () {
         const compradorNombre = document.getElementById("nombreComprador").value;
         const compradorApellido = document.getElementById("apellidoComprador").value;
         const compradorDni = document.getElementById("dniComprador").value;
-        const marcaVehiculo = document.getElementById("marcaVehiculo").value;
-        const modeloVehiculo = document.getElementById("modeloVehiculo").value;
-        const versionVehiculo = document.getElementById("versionVehiculo").value;
-        const anoVehiculo = document.getElementById("anoVehiculo").value;
-        const patenteVehiculo = document.getElementById("patenteVehiculo").value;
-        const chasisVehiculo = document.getElementById("chasisVehiculo").value;
-        const motorVehiculo = document.getElementById("motorVehiculo").value;
+        const compradorDireccion = document.getElementById("direccionComprador").value;
+        const compradorTelefono = document.getElementById("telefonoComprador").value;
+        const compradorEmail = document.getElementById("emailComprador").value;
+        const marcaVehiculo = document.getElementById("vehiculoMarca").value;
+        const modeloVehiculo = document.getElementById("vehiculoModelo").value;
+        const versionVehiculo = document.getElementById("vehiculoVersion").value;
+        const anoVehiculo = document.getElementById("vehiculoAno").value;
+        const patenteVehiculo = document.getElementById("vehiculoPatente").value;
+        const chasisVehiculo = document.getElementById("vehiculoChasis").value;
+        const motorVehiculo = document.getElementById("vehiculoMotor").value;
 
         // Reemplaza los marcadores de posición en el contrato
         document.getElementById("vendedorNombreCompleto").textContent = vendedorNombre + " " + vendedorApellido;
@@ -30,6 +37,10 @@ document.addEventListener("DOMContentLoaded", function () {
         document.getElementById("vendedorTelefono").textContent = vendedorTelefono;
         document.getElementById("vendedorEmail").textContent = vendedorEmail;
         document.getElementById("compradorNombreCompleto").textContent = compradorNombre + " " + compradorApellido;
+        document.getElementById("compradorDireccion").textContent = compradorDireccion;
+        document.getElementById("compradorTelefono").textContent = compradorTelefono;
+        document.getElementById("compradorEmail").textContent = compradorEmail;
+
         document.getElementById("marcaVehiculo").textContent = marcaVehiculo;
         document.getElementById("modeloVehiculo").textContent = modeloVehiculo;
         document.getElementById("versionVehiculo").textContent = versionVehiculo;
@@ -37,10 +48,16 @@ document.addEventListener("DOMContentLoaded", function () {
         document.getElementById("patenteVehiculo").textContent = patenteVehiculo;
         document.getElementById("chasisVehiculo").textContent = chasisVehiculo;
         document.getElementById("motorVehiculo").textContent = motorVehiculo;
+        document.getElementById('fechaCreacionContrato').textContent = dia+ "/" + mes + "/" + anio;
 
         // Muestra el contrato y el botón de imprimir
         contratoContainer.style.display = "block";
         imprimirContratoBtn.style.display = "block";
+
+        console.log("marcaVehiculo element:", document.getElementById("marcaVehiculo"));
+        console.log("marcaVehiculo value:", marcaVehiculo);
+        console.log(dia)
+
     });
 
     imprimirContratoBtn.addEventListener("click", function () {
